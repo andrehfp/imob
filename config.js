409d15486ef56ebd3968426c57ffcdab5b0456e7
André Prado <andre@pixelcart.com.br>
2022-01-05 17:52:33 -0300

    * Adiciona e busca na base imóveis de locação

--- config.js
+++ config.js
@@ -5,6 +5,8 @@ dotenv.config()
 
 const {
     PORT,
+    RAPID_API_HOST,
+    RAPID_API_KEY,
     API_KEY,
     AUTH_DOMAIN,
     PROJECT_ID,
@@ -17,6 +19,8 @@ assert(PORT, 'PORT is required')
 
 module.exports = {
     port: PORT,
+    rapid_api_host:RAPID_API_HOST,
+    rapid_api_key:RAPID_API_KEY,
     firebaseConfig: {
         apiKey:API_KEY,
         authDomain:AUTH_DOMAIN,
--- index.js
+++ index.js
@@ -11,8 +11,45 @@ const PORT = process.env.PORT || config.port
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`)) 
 
+var options = {
+    method: 'GET',
+    url: 'https://api-imoveis-locacao.p.rapidapi.com/conceito',
+    headers: {
+        'x-rapidapi-host': config.rapid_api_host,
+        'x-rapidapi-key': config.rapid_api_key
+    }
+};
 
+app.get('/busca', (req, res) => {
+    axios.request(options)
+        .then(function(response) {
+            imoveis = response.data
+            var batch = firestore.batch()
+            imoveis.forEach(imovel => {
+                var imovRef = firestore.collection('imoveis').doc()
+                batch.set(imovRef,imovel)
+            })
+            console.log('Imóveis: ', imoveis.length)
+            batch.commit()
+            res.json(imoveis)
+        })
+        .catch(function(error) {
+            console.error(error)
+        })
+})
 
+app.get('/db', (req, res) => {
+    var imoveis = []
+    const db = firestore.collection('imoveis')
+    const doc = db.get()
+        .then((snapshot)=> {
+            snapshot.forEach(doc => {
+                imoveis.push(doc.data())
+            })
+            console.log('Na base: ', imoveis.length)
+            res.json( imoveis)
 
+        })
+})
 
 
