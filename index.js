f8fbb2e364dad176ca91ee764701e30fea9ab388
André Prado <andre@pixelcart.com.br>
2022-01-06 17:21:59 -0300

    * Salva no banco de dados * Salva imóveis novos na collection 'novos'

--- index.js
+++ index.js
@@ -20,23 +20,63 @@ var options = {
     }
 };
 
-app.get('/busca', (req, res) => {
+app.get('/add', (req, res) => {
+    var imoveis = []
+    var existentes = []
+    var novos = []
+    var promises = []
+    const db = firestore.collection('imoveis')
     axios.request(options)
         .then(function(response) {
             imoveis = response.data
             var batch = firestore.batch()
             imoveis.forEach(imovel => {
-                var imovRef = firestore.collection('imoveis').doc()
-                batch.set(imovRef,imovel)
+                promises.push(
+                    db.doc(imovel.uid).get()
+                    .then(doc => {
+                        if (doc.exists) existentes.push(imovel) 
+                        else {
+                            novos.push(imovel)
+                            var imovRef = firestore.collection('imoveis').doc(imovel.uid)
+                            batch.set(imovRef,imovel)
+                        }
+                    }).catch(error => {
+                        console.log(error)
+                    })
+                )
             })
-            console.log('Imóveis: ', imoveis.length)
-            batch.commit()
-            res.json(imoveis)
+            Promise.all(promises)
+                .then((ok) => {
+                    res.json(imoveis)
+                    batch.commit()
+                    console.log('Imóveis da API: ', imoveis.length)
+                    console.log('Existentes: ', existentes.length)
+                    console.log('Novos: ', novos.length)
+                })
+                .then(()=> {
+                    addNew(novos)
+                })
+                .catch(error => {
+                    console.log(error)
+                })
         })
         .catch(function(error) {
             console.error(error)
         })
 })
+
+function addNew(novos){
+    console.log('novos imóveis para cadastro: ', novos.length)
+    db = firestore.collection('novos')
+    var batch = firestore.batch()
+    novos.forEach(imovel => {
+        imovel.createdAt = Date.now()
+        var imovRef = firestore.collection('novos').doc(imovel.uid)
+        batch.set(imovRef, imovel)
+    })
+    batch.commit()
+}
+
 
 app.get('/db', (req, res) => {
     var imoveis = []
@@ -47,9 +87,29 @@ app.get('/db', (req, res) => {
                 imoveis.push(doc.data())
             })
             console.log('Na base: ', imoveis.length)
-            res.json( imoveis)
-
+            res.json(imoveis)
         })
 })
 
+app.get('/novos', (req, res) => {
+    var imoveis = []
+    
+    const db = firestore.collection('novos')
+    const doc = db.get()
+        .then((snapshot) => {
+            snapshot.forEach(doc => {
+                imoveis.push(doc.data())
+            })
+        })
+        .then(() => {
+            res.json(imoveis)
+        })
+        .catch( err => {
+            console.log(err)    
+        })
 
+})
+
+app.get('/', (req, res) => {
+    res.json('Bem-vindo ao app Imob!')
+})
